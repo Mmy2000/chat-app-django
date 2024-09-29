cd00aee5dce99790eceef2ba8ddb5c8cc202d2f9
Mmy2000 <yahianada005@gmail.com>
2024-09-29 05:19:46 +0300

    fetch the endpiont

--- static/js/main.js
+++ static/js/main.js
@@ -29,9 +29,9 @@ const chatSubmitElement = document.querySelector('#chat_message_submit')
  * Functions 
  */
 
-function scrollToBottom() {
-    chatLogElement.scrollTop = chatLogElement.scrollHeight
-}
+// function scrollToBottom() {
+//     chatLogElement.scrollTop = chatLogElement.scrollHeight
+// }
 
 
 function getCookie(name) {
@@ -55,82 +55,82 @@ function getCookie(name) {
 }
 
 
-function sendMessage() {
-    chatSocket.send(JSON.stringify({
-        'type': 'message',
-        'message': chatInputElement.value,
-        'name': chatName
-    }))
+// function sendMessage() {
+//     chatSocket.send(JSON.stringify({
+//         'type': 'message',
+//         'message': chatInputElement.value,
+//         'name': chatName
+//     }))
 
-    chatInputElement.value = ''
-}
+//     chatInputElement.value = ''
+// }
 
 
-function onChatMessage(data) {
-    console.log('onChatMessage', data)
+// function onChatMessage(data) {
+//     console.log('onChatMessage', data)
 
-    if (data.type == 'chat_message') {
-        let tmpInfo = document.querySelector('.tmp-info')
+//     if (data.type == 'chat_message') {
+//         let tmpInfo = document.querySelector('.tmp-info')
 
-        if (tmpInfo) {
-            tmpInfo.remove()
-        }
+//         if (tmpInfo) {
+//             tmpInfo.remove()
+//         }
         
-        if (data.agent) {
-            chatLogElement.innerHTML += `
-                <div class="flex w-full mt-2 space-x-3 max-w-md">
-                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+//         if (data.agent) {
+//             chatLogElement.innerHTML += `
+//                 <div class="flex w-full mt-2 space-x-3 max-w-md">
+//                     <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
 
-                    <div>
-                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
-                            <p class="text-sm">${data.message}</p>
-                        </div>
+//                     <div>
+//                         <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+//                             <p class="text-sm">${data.message}</p>
+//                         </div>
                         
-                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
-                    </div>
-                </div>
-            `
-        } else {
-            chatLogElement.innerHTML += `
-                <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
-                    <div>
-                        <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
-                            <p class="text-sm">${data.message}</p>
-                        </div>
+//                         <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+//                     </div>
+//                 </div>
+//             `
+//         } else {
+//             chatLogElement.innerHTML += `
+//                 <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+//                     <div>
+//                         <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+//                             <p class="text-sm">${data.message}</p>
+//                         </div>
                         
-                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
-                    </div>
+//                         <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+//                     </div>
 
-                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
-                </div>
-            `
-        }
-    } else if (data.type == 'users_update') {
-        chatLogElement.innerHTML += '<p class="mt-2">The admin/agent has joined the chat!'
-    } else if (data.type == 'writing_active') {
-        if (data.agent) {
-            let tmpInfo = document.querySelector('.tmp-info')
+//                     <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+//                 </div>
+//             `
+//         }
+//     } else if (data.type == 'users_update') {
+//         chatLogElement.innerHTML += '<p class="mt-2">The admin/agent has joined the chat!'
+//     } else if (data.type == 'writing_active') {
+//         if (data.agent) {
+//             let tmpInfo = document.querySelector('.tmp-info')
 
-            if (tmpInfo) {
-                tmpInfo.remove()
-            }
+//             if (tmpInfo) {
+//                 tmpInfo.remove()
+//             }
 
-            chatLogElement.innerHTML += `
-                <div class="tmp-info flex w-full mt-2 space-x-3 max-w-md">
-                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+//             chatLogElement.innerHTML += `
+//                 <div class="tmp-info flex w-full mt-2 space-x-3 max-w-md">
+//                     <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
 
-                    <div>
-                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
-                            <p class="text-sm">The agent/admin is writing a message</p>
-                        </div>
-                    </div>
-                </div>
-            `
-        }
-    }
+//                     <div>
+//                         <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+//                             <p class="text-sm">The agent/admin is writing a message</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             `
+//         }
+//     }
 
-    scrollToBottom()
-}
+//     scrollToBottom()
+// }
 
 
 async function joinChatRoom() {
@@ -160,27 +160,27 @@ async function joinChatRoom() {
     })
 
 
-    chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomUuid}/`)
+    // chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomUuid}/`)
 
 
-    chatSocket.onmessage = function(e) {
-        console.log('onMessage')
+    // chatSocket.onmessage = function(e) {
+    //     console.log('onMessage')
 
-        onChatMessage(JSON.parse(e.data))
-    }
-
-
-    chatSocket.onopen = function(e) {
-        console.log('onOpen - chat socket was opened')
-
-        scrollToBottom()
-    }
+    //     onChatMessage(JSON.parse(e.data))
+    // }
 
 
-    chatSocket.onclose = function(e) {
+    // chatSocket.onopen = function(e) {
+    //     console.log('onOpen - chat socket was opened')
 
-        console.log('onClose - chat socket was closed')
-    }
+    //     scrollToBottom()
+    // }
+
+
+    // chatSocket.onclose = function(e) {
+
+    //     console.log('onClose - chat socket was closed')
+    // }
 }
 
 
@@ -210,26 +210,26 @@ chatJoinElement.onclick = function(e) {
 }
 
 
-chatSubmitElement.onclick = function(e) {
-    e.preventDefault()
+// chatSubmitElement.onclick = function(e) {
+//     e.preventDefault()
 
-    sendMessage()
+//     sendMessage()
 
-    return false
-}
-
-
-chatInputElement.onkeyup = function(e) {
-    if (e.keyCode == 13) {
-        sendMessage()
-    }
-}
+//     return false
+// }
 
 
-chatInputElement.onfocus = function(e) {
-    chatSocket.send(JSON.stringify({
-        'type': 'update',
-        'message': 'writing_active',
-        'name': chatName
-    }))
-}
+// chatInputElement.onkeyup = function(e) {
+//     if (e.keyCode == 13) {
+//         sendMessage()
+//     }
+// }
+
+
+// chatInputElement.onfocus = function(e) {
+//     chatSocket.send(JSON.stringify({
+//         'type': 'update',
+//         'message': 'writing_active',
+//         'name': chatName
+//     }))
+// }
